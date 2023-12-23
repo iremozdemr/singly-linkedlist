36ca67dc4aa4772c39778094404aab9a313fc7dc
İrem Özdemir <130483718+iremozdemr@users.noreply.github.com>
2023-12-23 13:25:09 +0300

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,162 +1,166 @@
-public class SinglyLinkedList<D>{
+public class SinglyLinkedList<E>{
 
-    private static class Node<D>{
+    //-----nested node class-----
+    private class Node<E>{
+        private E data; // data of the node
+        private Node<E> next; // reference to the next node
 
-        private D data;
-        private Node<D> nextN;
-
-        public Node(D data, Node<D> nextN){
+        // Node constructor
+        public Node(E data, Node<E> next){
             this.data = data;
-            this.nextN = nextN;
-        }
-
-        public D getData(){
-            return data;
-        }
-
-        public void setData(D data){
-            this.data = data;
-        }
-
-        public Node<D> getnextN(){
-            return nextN;
-        }
-
-        public void setNextN(Node<D> nextN){
-            this.nextN = nextN;
+            this.next = next;
         }
     }
+    //-----end of the nested node class-----
 
-    private Node<D> head = null;
-    private Node<D> tail = null;
-    private int size = 0;
+    private Node<E> head = null; // reference to the head of the list
+    private Node<E> tail = null; // reference to the tail of the list
+    private int size; // size of the list
 
-    @Override
-    public String toString() {
-        if(isEmpty()){
-            return null;
-        }
-        else if(size == 1){
-            return head.data.toString();
-        }
-        else{
-            Node<D> newHead = head;
-            String string= "";
-            for(int i = 0; i<size; i++){
-                string = string + " " + String.valueOf(newHead.data);
-                newHead = newHead.nextN;
-            }
-            return string;
-        }
+    // singlylinkedlist constructor
+    public SinglyLinkedList(){
     }
 
+    // checks if the list is empty
     public boolean isEmpty(){
-        return size == 0;
-    }
-
-    public D deleteByIndex(int index){
-        if(isEmpty() || index<0 || index>=size){
-            return null;
-        }
-        else if(index == 0){
-            return removeFirst();
-        }
-        else if(index == size-1){
-            return removeLast();
+        if(size == 0){
+            return true; // returns true if the list is empty
         }
         else{
-            Node<D> newHead = head;
-            for(int i = 0; i<index-1; i++){
-                newHead = newHead.nextN;
-            }
-            D removedD = newHead.nextN.data;
-            newHead.nextN = newHead.nextN.nextN;
-            size--;
-            return removedD; 
+            return false; // returns false if the list is not empty
         }
     }
 
-    public void addLast(D data){
+    // returns the size of the list
+    public int size(){
+        return size; // returns the size of the list
+    }
+
+    // returns the data of the first node in the list
+    public E first(){
         if(isEmpty()){
-            Node<D> newNode = new Node<D>(data,null);
-            head = newNode;
-            tail = newNode;
+            return null; // returns null if the list is empty
         }
-        else if(size == 1){
-            Node<D> newNode = new Node<D>(data,null);
-            head.nextN = newNode;
+        else{
+            return head.data; // returns the data of the first node
+        }
+    }
+
+    // returns the data of the last node in the list
+    public E last(){
+        if(isEmpty()){
+            return null; // returns null if the list is empty
+        }
+        else{
+            return tail.data; // returns the data of the last node
+        }
+    }
+
+    // adds a new node with data at the beginning of the list
+    public void addFirst(E newData){
+        if(isEmpty()){
+            Node<E> newNode = new Node<E>(newData,null);
+            head = newNode; // sets the new node as both head and tail
             tail = newNode;
         }
         else{
-            Node<D> newNode = new Node<D>(data,null);
-            tail.nextN = newNode;
-            tail.nextN.data = data;
-            tail = newNode;
-            tail.nextN = null;
+            Node<E> newNode = new Node<E>(newData,head);
+            head = newNode; // sets the new node as the new head
         }
-        size++;
+        size++; // increments the size of the list
     }
 
-    public void addFirst(D data){
+    // adds a new node with data at the end of the list
+    public void addLast(E newData){
         if(isEmpty()){
-            Node<D> newNode = new Node<D>(data,null);
-            head = newNode;
+            Node<E> newNode = new Node<E>(newData,null);
+            head = newNode; // sets the new node as both head and tail
             tail = newNode;
-        }
-        else if(size == 1){
-            Node<D> secondNode = head;
-            Node<D> newNode = new Node<D>(data,secondNode);
-            head = newNode;
         }
         else{
-            Node<D> newNode = new Node<D>(data,head);
-            head = newNode;
+            Node<E> newNode = new Node<E>(newData,null);
+            tail.next = newNode; // sets the new node as the next node of the tail
+            tail = newNode; // sets the new node as the new tail
         }
-        size++;
+        size++; // increments the size of the list
     }
 
-    public D removeLast(){
+    // removes and returns the data of the first node in the list
+    public E removeFirst(){
         if(isEmpty()){
-            return null;
+            return null; // returns null if the list is empty
         }
-        else if(size == 1){
-            D removedD = head.data;
+        else{
+            E removed = head.data; // stores the data of the first node
+            Node<E> newHead = head.next;
             head = null;
-            tail = null;
-            size--;
-            return removedD;
-        }
-        else{
-            Node<D> newHead = head;
-            Node<D> secondNode = null;
-            for(int i = 0; i<size-1; i++){
-                secondNode = newHead;
-                newHead = newHead.nextN;
-            }
-            D removedD = secondNode.nextN.data;
-            secondNode.nextN = null;
-            tail = secondNode;
-            size--;
-            return removedD;
+            head = newHead; // sets the next node as the new head
+            size--; // decrements the size of the list
+            return removed; // returns the data of the removed node
         }
     }
 
-    public D removeFirst(){
+    // removes and returns the data of the last node in the list
+    public E removeLast(){
         if(isEmpty()){
-            return null;
-        }
-        else if(size == 1){
-            D removedD = head.data;
-            head = null;
-            tail = null;
-            size--;
-            return removedD;
+            return null; // returns null if the list is empty
         }
         else{
-            D removeD = head.data;
-            head = head.nextN;
-            size--;
-            return removeD;
+            E removed = tail.data; // stores the data of the last node
+            Node<E> temp = head;
+            while(temp.next.next != null){
+                temp = temp.next;
+            }
+            tail = null;
+            tail = temp; // sets the current node as the new tail
+            temp.next = null; // removes the reference to the old tail
+            size--; // decrements the size of the list
+            return removed; // returns the data of the removed node
+        }
+    }
+
+    public E removeByIndex(int index){
+        E removed = null;
+        if(isEmpty()){
+            return removed;
+        }
+        else{
+            if(index == 0){
+                removed = removeFirst();
+            }
+            else if(index == size-1){
+                removed = removeLast();
+            }
+            else{
+                Node<E> temp = head;
+                for(int i = 0; i<size; i++){
+                    if(i+1 == index){
+                        removed = temp.next.data;
+                        temp.next = temp.next.next;
+                        size--;
+                        return removed;
+                    }
+                    temp = temp.next;
+                }
+            }
+        }
+        return removed;
+    }
+
+    // returns a string representation of the list
+    public String toString(){
+        String string = "";
+        if(isEmpty()){
+            return string; // returns an empty string if the list is empty
+        }
+        else{
+            Node<E> temp = head;
+            while(temp.next != null){
+                string = string +  " " + temp.data; // appends data to the string
+                temp = temp.next; // moves to the next node
+            }
+            string = string + " " + tail.data; // appends the last node's data
+            return string; // returns the string representation of the list
         }
     }
 }
